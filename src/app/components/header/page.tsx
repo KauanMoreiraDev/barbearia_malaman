6336e6d98611df0a4fc71c27c5f5831d43a97c01
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-07-11 00:22:17 -0300

    teste header responsivo

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -8,9 +8,9 @@ export default function Mheader() {
                 <header className="w-screen h-32 text-xl text-black bg-white flex justify-around items-center">
                     <Link href="#"><Image src={'/images/logo2.png'} alt="logo" width={120} height={120} className="rounded-full" /></Link>
                     <nav className="flex justify-end">
-                        <Link href="#" className="mx-8">Serviços</Link>
-                        <Link href="#" className="mx-8">Galeria</Link>
-                        <Link href="#" className="mx-8">Institucional</Link>
+                        <Link href="#" className="mx-2 sm:mx-8">Serviços</Link>
+                        <Link href="#" className="mx-2 sm:mx-8">Galeria</Link>
+                        <Link href="#" className="mx-2 sm:mx-8">Institucional</Link>
                     </nav>
                 </header>
             </div>
