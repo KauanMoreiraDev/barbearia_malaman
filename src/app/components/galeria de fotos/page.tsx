7f4f12da8b560158fbf0d8edb0159645d11d3e68
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-07-11 23:18:03 -0300

    estrutura ok

--- src/app/components/galeria de fotos/page.tsx
+++ src/app/components/galeria de fotos/page.tsx
@@ -21,14 +21,16 @@ export default function Galeria() {
     ]
 
     return (
-        <div className="grid grid-cols-3 gap-3 place-items-center place-content-end h-full bg-fuchsia-800">
+        <div className="grid grid-cols-1 gap-2 place-items-center w-full h-full bg-fuchsia-800">
+            <div className="grid sm:grid-cols-3 sm:gap-3">
             {fotos.map((ftfn, index) => (
                 <div key={index} className="bg-slate-950 border-orange-500 border w-40 h-40">
                     <div className="text-white">
                         {ftfn.nome}
                     </div>
                 </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
