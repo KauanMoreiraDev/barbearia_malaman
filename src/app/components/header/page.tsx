5dc1239017b5a698e417f7f7c94ef47322877e04
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-07-14 21:34:02 -0300

    onclick ok, bg dele ok, teste de links agr

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -13,12 +13,23 @@ export default function Mheader() {
             <div className="mx-auto max-w-screen-lg">
                 <div className="flex justify-between items-center">
                     <Link href="#"><Image src={'/images/logo2.png'} alt="logo" width={120} height={120} className="rounded-full" /></Link>
-                    <div className="block sm:hidden p-2 mx-3">
-                        <button onClick={() => setOpen(!open)} className="bg-lime-500 w-full h-full">{open ? <Image src={'/images/cardapio.png'} alt="abrir menu" width={30} height={30} /> : <div className="flex flex-col items-end"><Image src={'/images/x.png'} alt="fecha menu" width={30} height={30} /><div className="bg-lime-300 w-screen h-screen"> <nav className="flex flex-col justify-center">
-                                <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Serviços</Link>
-                                <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Galeria</Link>
-                                <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Institucional</Link>
-                            </nav></div></div>}</button>
+                    <div className="block sm:hidden p-2 mx-3 relative z-50">
+                        <button onClick={() => setOpen(!open)} className="w-full h-full">
+                            {open ? <Image src={'/images/cardapio.png'} alt="abrir menu" width={30} height={30} /> : <div className="flex flex-col items-end">
+                                <Image src={'/images/x.png'} alt="fecha menu" width={30} height={30} />
+                                <div className=" w-screen h-screen">
+                                    <div className="bg-gradient-to-b from-slate-600 to-slate-900 opacity-70 absolute inset-0">
+                                    </div>
+                                    <div className="text-black font-bold text-3xl">
+                                        <nav className="flex flex-col justify-center relative z-50 space-y-4">
+                                            <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Serviços</Link>
+                                            <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Galeria</Link>
+                                            <Link href="#" className="mx-2 hover:text-yellow-400 sm:mx-8">Institucional</Link>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>}
+                        </button>
                     </div>
                     <div className="hidden sm:block">
                         <div className="text-xl font-medium text-black">
